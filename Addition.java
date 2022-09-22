a611f7249bb3ba02aeaa959e44de8bbecaf467f8
sumanth1901 <sumanthmxworkz@gmail.com>
2022-09-22 11:57:16 +0530

    java files

---
+++ Addition.java
@@ -0,0 +1,30 @@
+public class Addition
+{
+	public static void main(String[] add)
+	{
+	add(56,89);
+	sub(64,8);
+	mul(91,54);
+	div(146,51);
+	}
+	
+	 static void add(int a,int b)
+	{
+	System.out.println(a+b);
+	}
+	static void sub(int a,int b)
+	{
+	System.out.println(a-b);
+	}
+	static void mul(int a,int b)
+	{
+	System.out.println(a*b);
+	}
+	static void div(int a,int b)
+	{
+	System.out.println(a/b);
+	}
+}	
+	
+
+	
---
+++ Arithmatic.java
@@ -0,0 +1,15 @@
+public class Arithmatic
+{
+	public static void main(String[] add)
+	{
+	All(10,20);
+	}
+	
+	 static void All(int a,int b)
+	{
+	System.out.println(a+b);
+	System.out.println(a-b);
+	System.out.println(a*b);
+	System.out.println(a/b);
+	}
+}
